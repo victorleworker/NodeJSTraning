81003a8bd4aa2221e092c37dd38f3781f0152d04
le hoang viet <viet.le.worker@gmail.com>
2018-08-27 17:07:27 +0700

    add

---
+++ node-chat-app/server/server.js
@@ -0,0 +1,9 @@
+﻿const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+    console.log('serveris up on port 3000');
+});
