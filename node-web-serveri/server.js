2a56acf9aa213aae7f0233a6d51d83d00c8e9f9a
le hoang viet <viet.le.worker@gmail.com>
2018-08-27 16:36:00 +0700

    b

---
+++ node-web-serveri/server.js
@@ -0,0 +1,69 @@
+﻿const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT||3000;
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+//app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+
+/*
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+})
+*/
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+});
+
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pateTitle: 'Projects'
+    })
+});
+
+///bad -send back json with errormessage
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+})
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
