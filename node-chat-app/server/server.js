bacbaf20023cd22f4d523f32263b5de6bab49747
le hoang viet <viet.le.worker@gmail.com>
2018-08-28 16:54:32 +0700

    add

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -6,7 +6,7 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 var server = http.createServer(app);
 var io = socketIO(server);
 
@@ -14,6 +14,27 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('new user connectted');
+    /*
+    socket.emit('newMessage', {
+        from: 'Join',
+        text: 'see you then',
+        createAt:123123
+    });*/
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+    socket.on('createMessage', (message,callback) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback();
+    });
+
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude,coords.longitude))
+    });
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
---
+++ node-chat-app/server/utils/message.js
@@ -0,0 +1,16 @@
+﻿var generateMessage = (from, text) => {
+    return {
+        from,
+        text,
+        createdAt: new Date().getTime()
+    };
+};
+
+var generateLocationMessage = (from, latitude, longtitude) => {
+    return {
+        from,
+        url: `https://www.google.com/maps?q=${latitude},${longtitude}`,
+        createdAt: new Date().getTime()
+    }
+};
+module.exports = { generateMessage, generateLocationMessage };
