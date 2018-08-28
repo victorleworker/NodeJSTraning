7e267d72f501f76812217b50cc5bf2899f29a14d
le hoang viet <viet.le.worker@gmail.com>
2018-08-28 10:37:27 +0700

    start socket io

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,9 +1,24 @@
 ﻿const path = require('path');
+const http = require('http');
 const express = require('express');
-const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io');
 
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
+
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(3000, () => {
-    console.log('serveris up on port 3000');
+
+io.on('connection', (socket) => {
+    console.log('new user connectted');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
+    console.log('serveris up on port' + port);
 });
